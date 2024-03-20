7aa0881f134a26f0bc4eb31342aa86d0e95309c2
mbaj <01178508@pw.edu.pl>
2024-03-20 12:30:23 +0100

    zad2

---
+++ cw2/zadania/gpio_led_2.py
@@ -0,0 +1,22 @@
+import gpio4
+import time
+
+gpio27 = gpio4.SysfsGPIO(27)
+gpio27.export = True # use the pin
+gpio27.direction = 'out' 
+
+for i in range(10):
+    gpio27.value = 0
+    time.sleep(0.5)
+    gpio27.value = 0.2
+    time.sleep(0.5)
+    gpio27.value = 0.5
+    time.sleep(0.5)
+    gpio27.value = 0.8
+    time.sleep(0.5)
+    gpio27.value = 0.1
+    time.sleep(0.5)
+
+
+
+gpio27.export = False # release the pin

0a712b11989de861f668bb347dc6a1a5f8debe82
mbaj <01178508@pw.edu.pl>
2024-03-20 14:41:43 +0100

    zad 5

---
+++ cw2/zadania/gpio_sensor.py
@@ -0,0 +1,24 @@
+import gpio4
+import time
+sensor = gpio4.SysfsGPIO(19)
+sensor.export = True
+sensor.direction = 'in'
+trigger = gpio4.SysfsGPIO(27)
+trigger.export = True
+trigger.direction = 'out'
+
+trigger.value = 0
+time.sleep(0.00001)
+trigger.value = 1
+time.sleep(0.00001)
+
+value = 0
+t = time.time()
+while True:
+    if(value != sensor.value):
+        d = time.time() - t
+        t = time.time()
+        print(d * 34300 / 2)
+        value = sensor.value
+
+sensor.export = False

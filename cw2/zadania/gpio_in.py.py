1e3d2c251035984e9a48582392e8a3c3a8f69f65
mbaj <01178508@pw.edu.pl>
2024-03-20 13:08:03 +0100

    zad3

---
+++ cw2/zadania/gpio_in.py.py
@@ -0,0 +1,17 @@
+import gpio4
+
+sw1 = gpio4.SysfsGPIO(18)
+sw1.export = True
+sw1.direction = 'in'
+diode = gpio4.SysfsGPIO(27)
+diode.export = True
+diode.direction = 'out'
+
+while True:
+    if sw1.value == 1:
+        diode.value = 1
+    else:
+        diode.value = 0
+
+sw1.export = False
+diode.export = False

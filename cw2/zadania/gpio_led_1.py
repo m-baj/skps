abbbe81bbbd6bb7f535520d745528d5601fdd97f
mbaj <01178508@pw.edu.pl>
2024-03-20 12:24:57 +0100

    fix

--- cw2/zadania/gpio_led_1.py
+++ cw2/zadania/gpio_led_1.py
@@ -5,7 +5,7 @@ gpio27.export = True # use the pin
 gpio27.direction = 'out' 
 
 for i in range(10):
-    gpio27.value = True
-    gpio27.value = False
+    gpio27.value = 0
+    gpio27.value = 1
 
 gpio27.export = False # release the pin

61715aea60b4fe6f98e343f0b71caa708e068d73
mbaj <01178508@pw.edu.pl>
2024-03-20 13:33:18 +0100

    zad4

--- cw2/zadania/gpio_snd.py
+++ cw2/zadania/gpio_snd.py
@@ -1,23 +1,31 @@
 import gpio4
 
 
+
+import gpio4, time
+from math import sin, pi
+ 
 frequencies = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25, 587.33, 659.25, 698.46, 783.99, 880.00, 987.77, 1046.50]
+one_duration = 1
+fill = 0.5
 
-def play_tone(pin, frequency, duration):
-    pwm = gpio4.PWM(pin)
-    pwm.export = True
-    pwm.period = int(1 / frequency * 1e9)
-    pwm.duty_cycle = int(pwm.period / 2)
-    pwm.enable = True
-    time.sleep(duration)
-    pwm.export = False
+buzzer = gpio4.SysfsGPIO(19)
+buzzer.export = True
+buzzer.direction = 'out'
 
 
-def main():
-    pin = 12
+try:
     for frequency in frequencies:
-        play_tone(pin, frequency, 0.5)
-    pwm.export = False
+        period = 1 / frequency
+        t = time.time()
+        while time.time() - t < one_duration:
+            if time.time() - t < fill * period:
+                buzzer.value = 1
+            else:
+                buzzer.value = 0
+except:
+    raise Exception
+finally:
+    buzzer.export = False
 
-if __name__ == '__main__':
-    main()
+

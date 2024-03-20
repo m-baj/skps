922d02b37dfadc7c9275aa23d571601314305d4c
mbaj <01178508@pw.edu.pl>
2024-03-20 14:49:07 +0100

    zad 5

--- cw2/zadania/gpio_sensor.py
+++ cw2/zadania/gpio_sensor.py
@@ -7,18 +7,26 @@ trigger = gpio4.SysfsGPIO(27)
 trigger.export = True
 trigger.direction = 'out'
 
-trigger.value = 0
-time.sleep(0.00001)
-trigger.value = 1
-time.sleep(0.00001)
+def measure(sensor, trigger):
+    trigger.value = 0
+    time.sleep(0.00001)
+    trigger.value = 1
+    time.sleep(0.00001)
+    trigger.value = 0
 
-value = 0
-t = time.time()
+    while sensor.value == 0:
+        pass
+
+    t = time.time()
+
+    while sensor.value == 1:
+        pass
+
+    return (time.time() - t) * 34300 / 2
+
+
 while True:
-    if(sensor.value==1):
-        d = time.time() - t
-        t = time.time()
-        print(d * 34300 / 2)
-        value = sensor.value
+   res = measure(sensor, trigger)
+   print(res)
 
 sensor.export = False

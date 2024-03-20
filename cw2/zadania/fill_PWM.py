e6a02d1622120735f8f45a6e74fbab4d59680b59
mbaj <01178508@pw.edu.pl>
2024-03-20 12:47:11 +0100

    zad2

---
+++ cw2/zadania/fill_PWM.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+MIN_PERCENT = 10
+MAX_PERCENT = 90
+
+
+def generate_variable_fill_pwm(frequency, fill):
+    period = 1 / (frequency * 10000)
+    t = np.arange(0, 1, period)
+    pwm_signal = np.zeros(len(t))
+    pwm_signal[: int(fill * len(t))] = 1
+    return t, pwm_signal
+
+
+def main():
+    t_full, pwm_full = [], []
+    for index, fill in enumerate(range(MIN_PERCENT, MAX_PERCENT, 5)):
+        t, pwm_signal = generate_variable_fill_pwm(1, fill / 100)
+        t += index
+        t_full = np.concatenate((t_full, t))
+        pwm_full = np.concatenate((pwm_full, pwm_signal))
+
+    t_full = np.concatenate((t_full, t_full + 16))
+    pwm_full = np.concatenate((pwm_full, pwm_full[::-1]))
--- cw2/zadania/gpio_led_2.py
+++ cw2/zadania/gpio_led_2.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import gpio4
 import time
 
@@ -5,18 +7,24 @@ gpio27 = gpio4.SysfsGPIO(27)
 gpio27.export = True # use the pin
 gpio27.direction = 'out' 
 
-for i in range(10):
-    gpio27.value = 0
-    time.sleep(0.5)
-    gpio27.value = 0.2
-    time.sleep(0.5)
-    gpio27.value = 0.5
-    time.sleep(0.5)
-    gpio27.value = 0.8
-    time.sleep(0.5)
-    gpio27.value = 0.1
-    time.sleep(0.5)
+MIN_PERCENT = 0
+MAX_PERCENT = 100
 
 
+def generate_variable_fill_pwm(frequency, fill):
+    period = 1 / (frequency * 10000)
+    t = np.arange(0, 1, period)
+    pwm_signal = np.zeros(len(t))
+    pwm_signal[: int(fill * len(t))] = 1
+    return pwm_signal
+
+
+def main():
+    for index, fill in enumerate(range(MIN_PERCENT, MAX_PERCENT, 10)):
+        pwm_signal = generate_variable_fill_pwm(1, fill / 100)
+        t = 1/len(pwm_signal)
+        for value in pwm_signal:
+            gpio27.value = value
+            time.sleep(t)
 
 gpio27.export = False # release the pin
